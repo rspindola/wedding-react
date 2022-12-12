8207ddf7385f6d4ff0162cc747253431f95497da
Renato Castro <rspindola@live.com>
2022-12-12 15:21:25 -0300

    fix: change photos order

--- src/components/Gallery/photos.js
+++ src/components/Gallery/photos.js
@@ -4,17 +4,17 @@ const unsplashLink = (id) =>
   `https://renatoejulianne.com/images/us/${id}.jpg`;
 
 const unsplashPhotos = [
-  { id: "Osq7UAVxIOI", width: 1080, height: 780 },
   { id: "RkBTPqPEGDo", width: 1080, height: 720 },
+  { id: "GbLS6YVXj0U", width: 1080, height: 780 },
+  { id: "ALrCdq-ui_Q", width: 854, height: 1280 },
+  { id: "1azAjl8FTnU", width: 1080, height: 1549 },
+  { id: "Dhmn6ete6g8", width: 1280, height: 854 },
+  { id: "Osq7UAVxIOI", width: 1080, height: 780 },
   { id: "Jztmx9yqjBw", width: 854, height: 1280 },
   { id: "heLWtuAN3c", width: 854, height: 1280 },
-  { id: "GbLS6YVXj0U", width: 1080, height: 780 },
   { id: "9CRd1J1rEOM", width: 1080, height: 720 },
   { id: "xOigCUcFdA8", width: 854, height: 1280 },
-  { id: "1azAjl8FTnU", width: 1080, height: 1549 },
-  { id: "ALrCdq-ui_Q", width: 854, height: 1280 },
   { id: "twukN12EN7c", width: 854, height: 1280 },
-  { id: "Dhmn6ete6g8", width: 1280, height: 854 },
   { id: "9UjEyzA6pP4", width: 1080, height: 1620 },
   { id: "X48pUOPKf7A", width: 1080, height: 780 },
   { id: "xKhtkhc9HbQ", width: 1080, height: 780 },
