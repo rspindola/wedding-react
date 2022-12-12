aaac7ef8bb093d6dd4e7ba81cda5444cfe3d7b86
Renato Castro <rspindola@live.com>
2022-12-12 15:10:23 -0300

    feat: add more photos

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './style.scss';
 
-const photos = [
-  'one.jpg',
-  'two.jpg',
-  'three.jpg',
-  'four.jpg',
-  'five.jpg',
-  'six.jpg',
-];
+import PhotoAlbum from "react-photo-album";
 
-export const Gallery = () => (
-  <section className='Gallery'>
-    <div className='head'>
-      <h2 className='title'>Galeria</h2>
-    </div>
-    <Container>
-      <Row>
-        {photos.map((photo, index) => (
-          <Col className='my-2' xs={12} md={6} lg={4}>
-            <img
-              src={`/images/${photo}`}
-              className='img-thumbnail hover-shadow rounded'
-              alt='...'
-            />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  </section>
-);
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import photos from "./photos";
+
+const slides = photos.map(({ src, width, height, images }) => ({
+  src,
+  width,
+  height,
+  srcSet: images.map((image) => ({
+    src: image.src,
+    width: image.width,
+    height: image.height,
+  })),
+}));
+
+
+export const Gallery = () => {
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <section className='Gallery'>
+      <div className='head'>
+        <h2 className='title'>Galeria</h2>
+      </div>
+      <Container>
+        <Row>
+          <PhotoAlbum photos={photos} layout="columns" targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+
+          <Lightbox
+            slides={slides}
+            open={index >= 0}
+            index={index}
+            close={() => setIndex(-1)}
+            // enable optional lightbox plugins
+            plugins={[Fullscreen, Slideshow, Zoom]}
+          />
+        </Row>
+      </Container>
+    </section>
+  );
+}
---
+++ src/components/Gallery/photos.js
@@ -0,0 +1,42 @@
+const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
+
+const unsplashLink = (id) =>
+  `http://localhost:3001/images/us/${id}.jpg`;
+
+const unsplashPhotos = [
+  { id: "Osq7UAVxIOI", width: 1080, height: 780 },
+  { id: "RkBTPqPEGDo", width: 1080, height: 720 },
+  { id: "Jztmx9yqjBw", width: 854, height: 1280 },
+  { id: "heLWtuAN3c", width: 854, height: 1280 },
+  { id: "GbLS6YVXj0U", width: 1080, height: 780 },
+  { id: "9CRd1J1rEOM", width: 1080, height: 720 },
+  { id: "xOigCUcFdA8", width: 854, height: 1280 },
+  { id: "1azAjl8FTnU", width: 1080, height: 1549 },
+  { id: "ALrCdq-ui_Q", width: 854, height: 1280 },
+  { id: "twukN12EN7c", width: 854, height: 1280 },
+  { id: "Dhmn6ete6g8", width: 1280, height: 854 },
+  { id: "9UjEyzA6pP4", width: 1080, height: 1620 },
+  { id: "X48pUOPKf7A", width: 1080, height: 780 },
+  { id: "xKhtkhc9HbQ", width: 1080, height: 780 },
+];
+
+const photos = unsplashPhotos.map((photo) => {
+  const width = breakpoints[0];
+  const height = (photo.height / photo.width) * width;
+
+  return {
+    src: unsplashLink(photo.id, width, height),
+    width,
+    height,
+    images: breakpoints.map((breakpoint) => {
+      const height = Math.round((photo.height / photo.width) * breakpoint);
+      return {
+        src: unsplashLink(photo.id, breakpoint, height),
+        width: breakpoint,
+        height,
+      };
+    }),
+  };
+});
+
+export default photos;
